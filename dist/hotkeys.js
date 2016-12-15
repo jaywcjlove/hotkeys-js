09e88ac7985b5a78fe7015a817c632752411234d
jaywcjlove <398188662@qq.com>
2016-12-15 18:16:26 +0800

    released v2.0.0

--- dist/hotkeys.js
+++ dist/hotkeys.js
@@ -1,5 +1,5 @@
 /*!
- * hotkeys-js v1.0.11
+ * hotkeys-js v2.0.0
  * A simple micro-library for defining and dispatching keyboard shortcuts. It has no dependencies.
  * 
  * Copyright (c) 2016 undefined
