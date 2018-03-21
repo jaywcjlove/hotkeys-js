6fe1c728a730e935140b780c0c656968b9d2fecf
jaywcjlove <398188662@qq.com>
2018-03-22 01:26:29 +0800

    Add test case.

--- test/run.test.js
+++ test/run.test.js
@@ -17,8 +17,10 @@ function __triggerKeyboardEvent(el, keyCode, opt) {
   if (eventObj.initEvent) {
     eventObj.initEvent("keydown", true, true);
   }
-  eventObj.keyCode = keyCode;
-  eventObj.which = keyCode;
+  if (keyCode) {
+    eventObj.keyCode = keyCode;
+    eventObj.which = keyCode;
+  }
   if (opt) {
     for (var a in opt) {
       eventObj[a] = opt[a];
@@ -32,7 +34,7 @@ beforeAll(async () => {
   page = await browser.newPage();
 })
 
-describe('Alibaba Search', () => {
+describe('\n   Hotkeys.js Test Case.\n', () => {
   test('HTML loader', async () => {
     await page.goto('file://' + path.resolve('./test/index.html'), { waitUntil: 'networkidle2' });
   }, 10000);
@@ -44,12 +46,22 @@ describe('Alibaba Search', () => {
     expect(text).toBe('hotkeys');
   })
 
-  test('HotKeys Test Case', async () => {
-    hotkeys('w',  (e) => {
-      expect(e.keyCode).toBe(87);
+  test('HotKeys getPressedKeyCodes Test Case', async () => {
+    await hotkeys('command+ctrl+shift+a', function (e) {
+      expect(e.metaKey).toBeTruthy();
+      expect(e.ctrlKey).toBeTruthy();
+      expect(e.shiftKey).toBeTruthy();
+      expect(hotkeys.getPressedKeyCodes()[0]).toBe(65);
+    })
+    await __triggerKeyboardEvent(document.body, 65, {
+      metaKey: true,
+      ctrlKey: true,
+      shiftKey: true,
     });
-    __triggerKeyboardEvent(document.body, 87);
+    await hotkeys.unbind("command+ctrl+shift+a");
+  })
 
+  test('HotKeys Special keys Test Case', async () => {
     hotkeys('enter', function (e) {
       expect(e.keyCode).toBe(13);
     });
@@ -57,91 +69,117 @@ describe('Alibaba Search', () => {
       expect(e.keyCode).toBe(13);
     });
     __triggerKeyboardEvent(document.body, 13);
+    hotkeys.unbind("return");
+    hotkeys.unbind("enter");
 
     hotkeys('space', function (e) {
       expect(e.keyCode).toBe(32);
     });
     __triggerKeyboardEvent(document.body, 32);
+    hotkeys.unbind("space");
+
+    hotkeys('insert,ins', function (e) {
+      expect(e.keyCode).toBe(45);
+    });
+    __triggerKeyboardEvent(document.body, 45);
+    hotkeys.unbind("insert");
+    hotkeys.unbind("ins");
 
     hotkeys('backspace', function (e) {
       expect(e.keyCode).toBe(8);
     });
     __triggerKeyboardEvent(document.body, 8);
+    hotkeys.unbind("backspace");
 
     hotkeys('tab', function (e) {
       expect(e.keyCode).toBe(9);
     });
     __triggerKeyboardEvent(document.body, 9);
+    hotkeys.unbind("tab");
 
     hotkeys('clear', function (e) {
       expect(e.keyCode).toBe(12);
     });
     __triggerKeyboardEvent(document.body, 12);
+    hotkeys.unbind("clear");
 
     hotkeys(',', function (e) {
       expect(e.keyCode).toBe(188);
     });
     __triggerKeyboardEvent(document.body, 188);
+    hotkeys.unbind(",");
 
     hotkeys('.', function (e) {
       expect(e.keyCode).toBe(190);
     });
     __triggerKeyboardEvent(document.body, 190);
+    hotkeys.unbind(".");
 
     hotkeys('/', function (e) {
       expect(e.keyCode).toBe(191);
     });
     __triggerKeyboardEvent(document.body, 191);
+    hotkeys.unbind("/");
 
     hotkeys('`', function (e) {
       expect(e.keyCode).toBe(192);
     });
     __triggerKeyboardEvent(document.body, 192);
+    hotkeys.unbind("`");
 
     hotkeys('-', function (e) {
       expect(e.keyCode).toBe(isff ? 173 : 189);
     });
     __triggerKeyboardEvent(document.body, isff ? 173 : 189);
+    hotkeys.unbind("-");
 
     hotkeys('=', function (e) {
       expect(e.keyCode).toBe(isff ? 61 : 187);
     });
     __triggerKeyboardEvent(document.body, isff ? 61 : 187);
+    hotkeys.unbind("=");
 
     hotkeys(';', function (e) {
       expect(e.keyCode).toBe(isff ? 59 : 186);
     });
     __triggerKeyboardEvent(document.body, isff ? 59 : 186);
+    hotkeys.unbind(";");
 
     hotkeys("\'".toString(), function (e) {
       expect(e.keyCode).toBe(222);
     });
     __triggerKeyboardEvent(document.body, 222);
+    hotkeys.unbind("\'");
 
     hotkeys("\\".toString(), function (e) {
       expect(e.keyCode).toBe(220);
     });
     __triggerKeyboardEvent(document.body, 220);
+    hotkeys.unbind("\\");
 
     hotkeys("[".toString(), function (e) {
       expect(e.keyCode).toBe(219);
     });
     __triggerKeyboardEvent(document.body, 219);
+    hotkeys.unbind("[");
 
     hotkeys("]".toString(), function (e) {
       expect(e.keyCode).toBe(221);
     });
     __triggerKeyboardEvent(document.body, 221);
+    hotkeys.unbind("]");
 
     hotkeys('left', function (e) {
       expect(e.keyCode).toBe(37);
     });
     __triggerKeyboardEvent(document.body, 37);
+    hotkeys.unbind("left");
 
     hotkeys('up', function (e) {
       expect(e.keyCode).toBe(38);
     });
     __triggerKeyboardEvent(document.body, 38);
+    hotkeys.unbind("up");
 
     hotkeys('del', function (e) {
       expect(e.keyCode).toBe(46);
@@ -150,36 +188,44 @@ describe('Alibaba Search', () => {
       expect(e.keyCode).toBe(46);
     });
     __triggerKeyboardEvent(document.body, 46);
+    hotkeys.unbind("delete");
+    hotkeys.unbind("del");
 
     hotkeys('home', function (e) {
       expect(e.keyCode).toBe(36);
     });
     __triggerKeyboardEvent(document.body, 36);
+    hotkeys.unbind("home");
 
     hotkeys('pageup', function (e) {
       expect(e.keyCode).toBe(33);
     });
     __triggerKeyboardEvent(document.body, 33);
+    hotkeys.unbind("pageup");
 
     hotkeys('pagedown', function (e) {
       expect(e.keyCode).toBe(34);
     });
     __triggerKeyboardEvent(document.body, 34);
+    hotkeys.unbind("pagedown");
 
     hotkeys('end', function (e) {
       expect(e.keyCode).toBe(35);
     });
     __triggerKeyboardEvent(document.body, 35);
+    hotkeys.unbind("end");
 
     hotkeys('right', function (e) {
       expect(e.keyCode).toBe(39);
     });
     __triggerKeyboardEvent(document.body, 39);
+    hotkeys.unbind("right");
 
     hotkeys('down', function (e) {
       expect(e.keyCode).toBe(40);
     });
     __triggerKeyboardEvent(document.body, 40);
+    hotkeys.unbind("down");
 
     hotkeys('esc', function (e) {
       expect(e.keyCode).toBe(27);
@@ -188,7 +234,8 @@ describe('Alibaba Search', () => {
       expect(e.keyCode).toBe(27);
     });
     __triggerKeyboardEvent(document.body, 27);
-
+    hotkeys.unbind("esc");
+    hotkeys.unbind("escape");
 
     hotkeys('CapsLock', function (e) {
       expect(e.keyCode).toBe(20);
@@ -197,9 +244,32 @@ describe('Alibaba Search', () => {
       expect(e.keyCode).toBe(20);
     });
     __triggerKeyboardEvent(document.body, 20);
-
+    hotkeys.unbind("⇪");
+    hotkeys.unbind("CapsLock");
   })
 
+
+  test('HotKeys Test Case', async () => {
+    hotkeys('w',  (e) => {
+      expect(e.keyCode).toBe(87);
+    });
+    __triggerKeyboardEvent(document.body, 87);
+    hotkeys.unbind("w");
+
+    hotkeys('b',  (e) => {
+      expect(e.keyCode).toBe(66);
+    });
+    __triggerKeyboardEvent(document.body, 66);
+    hotkeys.unbind("b");
+
+    await hotkeys('a', async () => {
+      await expect(hotkeys.isPressed('a')).toBeTruthy();
+      await expect(hotkeys.isPressed('A')).toBeTruthy();
+      await expect(hotkeys.isPressed(65)).toBeTruthy();
+    });
+    __triggerKeyboardEvent(document.body, 65);
+    hotkeys.unbind("a");
+  })
 
   test('HotKeys Key combination Test Case', async () => {
 
@@ -243,6 +313,7 @@ describe('Alibaba Search', () => {
     __triggerKeyboardEvent(document.body, 65, {
       metaKey: true
     });
+    hotkeys.unbind("⌘+a");
 
     hotkeys('⌃+a', function (e) {
       expect(e.keyCode).toBe(65);
@@ -295,26 +366,97 @@ describe('Alibaba Search', () => {
     __triggerKeyboardEvent(document.body, 65, {
       ctrlKey: true
     });
-    hotkeys.unbind("⌃+a")
+    hotkeys.unbind("⌃+a");
+  })
 
-    hotkeys('⌃+a', 'scope1', function (e) {
+  // const _modifier = { //修饰键
+  //   '⇧': 16, shift: 16,
+  //   '⌥': 18, alt: 18, option: 18,
+  //   '⌃': 17, ctrl: 17, control: 17,
+  //   '⌘': isff ? 224 : 91, cmd: isff ? 224 : 91, command: isff ? 224 : 91
+  // };
+
+  test('HotKeys modifier key ⌘,cmd,command Test Case', async () => {
+    await __triggerKeyboardEvent(document.body, 65, {
+      shiftKey: true
+    });
+    await hotkeys.unbind('shift+a');
+  });
+
+  test('HotKeys modifier key ⌘,cmd,command Test Case', async () => {
+    await hotkeys('*', function (e) {
+      expect(e.keyCode).toBe(isff ? 224 : 91);
+    });
+    await __triggerKeyboardEvent(document.body, isff ? 224 : 91);
+    await hotkeys.unbind('*');
+  });
+
+  test('HotKeys modifier key ⌃,ctrl,control Test Case', async () => {
+    await hotkeys('*', function (e) {
+      expect(e.keyCode).toBe(17);
+    });
+    await __triggerKeyboardEvent(document.body, 17);
+    await hotkeys.unbind('*');
+  });
+
+  test('HotKeys modifier key ⌥,alt,option Test Case', async () => {
+    await hotkeys('*', function (e) {
+      expect(e.keyCode).toBe(18);
+    });
+    await __triggerKeyboardEvent(document.body, 18);
+    await hotkeys.unbind('*');
+  });
+
+  test('HotKeys modifier key ⇧,shift Test Case', async () => {
+    await hotkeys('*', function (e) {
+      expect(e.keyCode).toBe(16);
+    });
+    await __triggerKeyboardEvent(document.body, 16);
+    await hotkeys.unbind('*');
+  });
+
+  test('HotKeys modifier scope,setScope,getScope,deleteScope Test Case', async () => {
+
+    await hotkeys('⌃+a', 'scope1', async (e) => {
       expect(e.keyCode).toBe(65);
       expect(hotkeys.getScope()).toBe('scope1');
+      await hotkeys.deleteScope('scope1');
     });
-    hotkeys.setScope('scope1');
-    __triggerKeyboardEvent(document.body, 65, {
+    await hotkeys.setScope('scope1');
+    await __triggerKeyboardEvent(document.body, 65, {
       ctrlKey: true
     });
 
-    hotkeys('⌃+a', 'scope2', function (e) {
+    await hotkeys('⌃+a', 'scope2', async (e) => {
       expect(e.keyCode).toBe(65);
       expect(hotkeys.getScope()).toBe('scope2');
+      await hotkeys.deleteScope('scope2');
     });
-    hotkeys.setScope('scope2');
-    __triggerKeyboardEvent(document.body, 65, {
+    await hotkeys.setScope('scope2');
+    await __triggerKeyboardEvent(document.body, 65, {
       ctrlKey: true
     });
+
+    await hotkeys('⌃+a', 'scope3', async (e) => {
+      expect(e.keyCode).toBe(65);
+      expect(hotkeys.getScope()).toBe('scope3');
+      await hotkeys.deleteScope('scope3');
+    });
+    await hotkeys.setScope('scope3');
+    await __triggerKeyboardEvent(document.body, 65, {
+      ctrlKey: true
+    });
+    expect(hotkeys.getScope()).toBe('all');
   })
+
+  test('HotKeys modifier noConflict Test Case', async () => {
+    const keys = await hotkeys.noConflict();
+    await keys('a', function (e) {
+      expect(e.keyCode).toBe(65);
+      expect(e.which).toBe(65);
+    });
+    await __triggerKeyboardEvent(document.body, 65);
+  });
 
   afterAll(async () => {
     await browser.close()
