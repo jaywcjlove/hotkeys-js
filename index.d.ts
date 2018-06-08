3f9ff1e95798c2b7d044a9a5bc5fa766330e8bc9
Eager Wei <1226393396@qq.com>
2018-06-08 10:13:29 +0800

    add Type definitions

---
+++ index.d.ts
@@ -0,0 +1,58 @@
+// Type definitions for hotkeys v3.3.3
+// Project: https://github.com/jaywcjlove/hotkeys
+// Definitions by: Eager Wei <https://github.com/weidapao>
+
+interface HotkeysEvent {
+  key: string
+  method: KeyHandler
+  mods: number[]
+  scope: string
+  shortcut: string
+}
+
+interface KeyHandler {
+  (keyboardEvent: KeyboardEvent, hotkeysEvent: HotkeysEvent): void
+}
+
+interface FilterEvent {
+  target?: {
+    tagName?: string
+  }
+  srcElement?: {
+    tagName?: string
+  }
+}
+
+interface Hotkeys {
+  (key: string, callback: KeyHandler): void
+  (key: string, scope: string, callback: KeyHandler): void
+
+  shift: boolean
+  ctrl: boolean
+  alt: boolean
+  option: boolean
+  control: boolean
+  cmd: boolean
+  command: boolean
+
+  setScope(scopeName: string): void
+  getScope(): string
+  deleteScope(scopeName: string): void
+
+  noConflict(): void
+
+  unbind(key: string): void
+  unbind(key: string, scopeName: string): void
+
+  isPressed(keyCode: number): boolean
+  isPressed(keyCode: string): boolean
+  getPressedKeyCodes(): number[]
+
+  filter(event: FilterEvent): boolean
+}
+
+declare var hotkeys: Hotkeys
+
+declare module 'hotkeys' {
+  export = hotkeys
+}
