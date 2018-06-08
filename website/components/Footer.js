85c8f1b39900edb00ebb16a0ae999da7d91d473b
jaywcjlove <398188662@qq.com>
2018-06-08 11:26:14 +0800

    Fix Document code detection issues.

--- website/components/Footer.js
+++ website/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer({ name, href, year }) {
     <div className={style.footer}>
       <div>Licensed under MIT. (Yes it&acute;s free and <a href="https://github.com/jaywcjlove/hotkeys">open-sourced</a>)</div>
       <div>
-      © <a target="_blank" href={href}>{name}</a> {year}
+        © <a target="_blank" rel="noopener noreferrer" href={href}>{name}</a> {year}
       </div>
     </div>
   );
--- website/components/GithubCorner.js
+++ website/components/GithubCorner.js
@@ -6,7 +6,7 @@ export default function GithubCorner({ url }) {
     return null;
   }
   return (
-    <a href={url} target="_blank" className={styles.corner}>
+    <a href={url} target="_blank" rel="noopener noreferrer" className={styles.corner}>
       <svg viewBox="0 0 250 250">
         <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
         <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style={{ transformOrigin: '130px 106px' }} className="octo-arm" />
