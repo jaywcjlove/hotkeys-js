e2ebb94b71e0a80a3766bd5efde86fa305be484e
jaywcjlove <398188662@qq.com>
2018-09-20 17:08:50 +0800

    Update watch script.

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,13 +1,39 @@
 const path = require('path');
 const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const banner = require('bannerjs');
 require('colors-cli/toxic');
 
 const watchOptions = {
   input: 'src/main.js',
+  plugins: [
+    nodeResolve(), // so Rollup can find `ms`
+    commonjs(), // so Rollup can convert `ms` to an ES module
+    babel({
+      exclude: 'node_modules/**', // 只编译我们的源代码
+    }),
+  ],
   output: [
-    { file: 'dist/hotkeys.common.js', name: 'hotkeys', format: 'cjs' },
-    { file: 'dist/hotkeys.js', name: 'hotkeys', format: 'umd' },
-    { file: 'dist/hotkeys.esm.js', name: 'hotkeys', format: 'es' },
+    {
+      file: 'dist/hotkeys.common.js',
+      name: 'hotkeys',
+      banner: banner.multibanner(),
+      format: 'cjs',
+    },
+    {
+      file: 'dist/hotkeys.js',
+      name: 'hotkeys',
+      banner: banner.multibanner(),
+      format: 'umd',
+    },
+    {
+      file: 'dist/hotkeys.esm.js',
+      name: 'hotkeys',
+      banner: banner.multibanner(),
+      format: 'es',
+    },
   ],
 };
 const watcher = rollup.watch(watchOptions);
