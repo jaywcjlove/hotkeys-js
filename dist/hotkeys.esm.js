30efd76a4c561871a58acfe845d9542ddf99e89f
jaywcjlove <398188662@qq.com>
2019-05-09 13:00:01 +0800

    released v3.6.7 #73

--- dist/hotkeys.esm.js
+++ dist/hotkeys.esm.js
@@ -1,5 +1,5 @@
 /*!
- * hotkeys-js v3.6.6
+ * hotkeys-js v3.6.7
  * A simple micro-library for defining and dispatching keyboard shortcuts. It has no dependencies.
  * 
  * Copyright (c) 2019 kenny wong <wowohoo@qq.com>
