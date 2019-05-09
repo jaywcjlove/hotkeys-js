cb854e407c2528bada152599fae22c71b866acbd
jaywcjlove <398188662@qq.com>
2019-05-09 16:57:09 +0800

    released v3.6.8 #74

--- dist/hotkeys.js
+++ dist/hotkeys.js
@@ -1,5 +1,5 @@
 /*!
- * hotkeys-js v3.6.7
+ * hotkeys-js v3.6.8
  * A simple micro-library for defining and dispatching keyboard shortcuts. It has no dependencies.
  * 
  * Copyright (c) 2019 kenny wong <wowohoo@qq.com>
