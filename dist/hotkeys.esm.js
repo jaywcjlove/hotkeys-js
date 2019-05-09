636c40af0343af0455d615c9f6a0ac333b112e46
余君强 <yujunqiang@100tal.com>
2019-05-09 12:30:55 +0800

    feat: modify filter function on readOnly element

--- dist/hotkeys.esm.js
+++ dist/hotkeys.esm.js
@@ -157,13 +157,19 @@ function getScope() {
 function getPressedKeyCodes() {
   return _downKeys.slice(0);
 } // 表单控件控件判断 返回 Boolean
+// hotkey is effective only when filter return true
 
 
 function filter(event) {
   var target = event.target || event.srcElement;
-  var tagName = target.tagName; // 忽略这些情况下快捷键无效
+  var tagName = target.tagName;
+  var flag = true; // ignore: isContentEditable === 'true', <input> and <textarea> when readOnly state is false, <select>
 
-  return !(tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA' || target.isContentEditable);
+  if (target.isContentEditable || tagName === 'TEXTAREA' || (tagName === 'INPUT' || tagName === 'TEXTAREA') && !target.readOnly) {
+    flag = false;
+  }
+
+  return flag;
 } // 判断摁下的键是否为某个键，返回true或者false
 
 
