d3c0d4830b2fead833288ea608513a354253a547
James Chao <jameschao@gmail.com>
2020-03-27 22:18:41 +0800

    Allow boolean return value from KeyHandler (#164)

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,7 @@ export interface HotkeysEvent {
 }
 
 export interface KeyHandler {
-  (keyboardEvent: KeyboardEvent, hotkeysEvent: HotkeysEvent): void
+  (keyboardEvent: KeyboardEvent, hotkeysEvent: HotkeysEvent): void | boolean
 }
 
 type Options = {
