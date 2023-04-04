bb7481ec21fcf0d8a8d9c34a028c88a849728ee3
Selphine <selphine@selphine.com>
2023-04-05 01:01:39 +0800

    type: add `getPressedKeyString` to `index.d.ts` for #399 (#424)

--- index.d.ts
+++ index.d.ts
@@ -52,6 +52,7 @@ export interface Hotkeys {
   isPressed(keyCode: number): boolean;
   isPressed(keyCode: string): boolean;
   getPressedKeyCodes(): number[];
+  getPressedKeyString(): string[];
 
   filter(event: KeyboardEvent): boolean;
 }
