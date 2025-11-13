8f31d8344684fddfec4107597aadcac90d34b926
Nikita Nafranets <root@dimensi.dev>
2025-11-14 00:51:20 +0300

    feat: Extend JSX support for custom 'dark-mode' element
    This commit adds TypeScript declarations for a new custom JSX element, 'dark-mode', within the React namespace. The declaration includes optional properties for enhanced functionality. These changes improve type safety and enable better integration of the 'dark-mode' component in the project.

--- website/vite-env.d.ts
+++ website/vite-env.d.ts
@@ -9,3 +9,18 @@ declare module '*.css' {
   const content: string;
   export default content;
 }
+
+import 'react';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'dark-mode': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          permanent?: boolean;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
